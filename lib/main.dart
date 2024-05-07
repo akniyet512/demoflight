25ac4c25f92cbd0c63ed0c2c211f985aa668b6b4
Akniyet Begimkulov <akniyetbegimkulov@gmail.com>
2024-05-07 12:50:49 +0500

    basic structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,29 @@
+import 'dart:io';
+
+import 'package:demoflight/app/app_notifier.dart';
+import 'package:demoflight/app/app_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:url_strategy/url_strategy.dart';
+import 'package:demoflight/utilities/my_http_overrides.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+//flutter run -d chrome --web-renderer html --web-port 5000 --web-browser-flag "--disable-web-security"
+void main() async {
+  //enables main to be async
+  WidgetsFlutterBinding.ensureInitialized();
+  //removes HandshakeException
+  HttpOverrides.global = MyHttpOverrides();
+  //removes # in root route
+  setPathUrlStrategy();
+  //model init
+  final AppNotifier model = AppNotifier();
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+  await model.setData().whenComplete(() {
+    runApp(
+      ChangeNotifierProvider(
+        create: (context) => model,
+        child: const AppWidget(),
       ),
-      home: const MyHomePage(),
     );
-  }
-}
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
+  });
 }
